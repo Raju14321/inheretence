c651abee818ab7af3c9d8f8455099b1e4713f204
Raju14321 <100340131+Raju14321@users.noreply.github.com>
2022-03-01 09:40:18 +0530

    Add files via upload

---
+++ inharitance/Program1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace inharitance
+{
+    class Program1
+    {
+       public  void Test()
+        {
+            Console.WriteLine("Jacob");
+        }
+        public void Test2()
+        {
+            Console.WriteLine("lucky");
+        }
+        //public Program1 ()
+        //{
+        //    Console.WriteLine("Names");
+        //}
+    }
+}
---
+++ inharitance/program2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace inharitance
+{
+    class program2:Program1
+    {
+        public  void Test3()
+        {
+            Console.WriteLine("Raju");
+        }
+        static void Main()
+        {
+            program2 c = new program2();
+            c.Test();
+            c.Test2();
+            c.Test3();
+            Console.ReadLine();
+
+        }
+    }
+}
